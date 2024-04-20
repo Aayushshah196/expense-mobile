0b918bea1229eef57e15b247f5b20d9cdb0fbdaa
aayushshah.kanu <aayushshah.kanu@docsumo.com>
2024-04-20 09:19:12 +0545

    Added currentuser in the context information

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -4,9 +4,17 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        currentUser,
+        setCurrentUser,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
